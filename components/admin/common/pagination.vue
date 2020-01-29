58ff3f5f3514cb774ec91d373ce4714852bf312c
Abdel Aziz hassan <zizo1999988@mgail.com>
2020-01-29 23:36:03 +0200

    seprate pagenation
    seperate pagination in component

---
+++ components/admin/common/pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-pagination
+    v-if="response.payload.total_page > 1"
+    :total="response.payload.total_record"
+    :current.sync="response.payload.page"
+    :range-before="4"
+    :range-after="4"
+    :rounded="true"
+    :per-page="response.payload.limit"
+    icon-prev="chevron-left"
+    icon-next="chevron-right"
+    aria-next-label="Next page"
+    aria-previous-label="Previous page"
+    aria-page-label="Page"
+    aria-current-label="Current page"
+    @change="changePage"
+  >
+  </b-pagination>
+</template>
+<script>
+  export default {
+    props: ["response"],
+    methods: {
+      changePage(value) {
+        this.$emit("changePage", value)
+      }
+    }
+  }
+</script>
